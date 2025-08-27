67bbd0ff33432c37aa157c58ab175b459dbfdfac
Muhammad Wasim Uddin Omar <mdwasimu015@gmail.com>
2025-08-27 07:49:33 +0600

    Inshallah

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    appDir: true  // App Router enable
+  },
+  reactStrictMode: true, // optional but recommended
+}
 
 export default nextConfig;
